60836aa538ad69a3fdfaadeee950083e4a9dbf0b
Renato Bruschi <renatomassocato10@gmail.com>
2020-01-09 03:49:42 -0300

    History added

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace gitTest
 	    cm = Console.ReadLine();
 	    Console.WriteLine("Queria que o " + name + " comesse meu cuzinho todos os dias");
 	    Console.WriteLine("Sua piroca de " + cm + "cm me deixa louco para mamar e sentar nele");
-	    Console.ReadLine();
+        Console.WriteLine(name + " gosta de me provocar. Isso me faz achar que ele quer me comer... Ele tem o costume de pegar na minha bunda, pegar no pau dele repentinamente na minha frente e até dá tapas na minha bunda de vez enquando. Ele faz isso por que sabe que me deixa louco, já fiquei até de pau duro uma vez e quando ele percebeu falou: Eu te deixo duro assim ? Com aquele olhar sexy dele...");
+        Console.ReadLine();
         }
     }
 }
